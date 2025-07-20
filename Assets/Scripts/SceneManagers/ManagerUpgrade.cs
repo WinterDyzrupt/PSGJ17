fc18003fb2491865916baf7b4600c0380737f081
Winter Dyzrupt <Winter@DyzruptGames.com>
2025-07-20 15:09:47 -0700

    commited some changes to see if it gets my comments out of pending.

--- Assets/Scripts/SceneManagers/ManagerUpgrade.cs
+++ Assets/Scripts/SceneManagers/ManagerUpgrade.cs
@@ -3,93 +3,96 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class ManagerUpgrade : MonoBehaviour
+namespace SceneManagers
 {
-    public static ManagerUpgrade Instance;
-    public UpgradeButton[] UpgradeButtons;
-    public UpgradeProgress selectedUpgrade;
-    public TMP_Text FameAmount;
-    public TMP_Text SelectedName;
-    public TMP_Text SelectedDescription;
-    public Button PurchaseButton;
-    public CanvasGroup[] panels;
-    public Button[] toggleButtons;
-    public Image[] tabImages;
-
-    void Start()
+    public class ManagerUpgrade : MonoBehaviour
     {
-        Instance = this;
-        UpdateButtons();
-        UpdateUpgradeScene();
-    }
-    public void LoadTitleScene()
-    {
-        SceneManager.LoadScene("Title");
-    }
+        public static ManagerUpgrade Instance;
+        public UpgradeButton[] upgradeButtons;
+        public UpgradeProgress selectedUpgrade;
+        public TMP_Text fameAmount;
+        public TMP_Text selectedName;
+        public TMP_Text selectedDescription;
+        public Button purchaseButton;
+        public CanvasGroup[] panels;
+        public Button[] toggleButtons;
+        public Image[] tabImages;
 
-    private void UpdateButtons()
-    {
-        foreach (UpgradeButton button in UpgradeButtons) button.UpdateButton();
-    }
-
-
-    public void SelectUpgrade(UpgradeProgress upgradeProgress)
-    {
-        selectedUpgrade = upgradeProgress;
-        UpdateUpgradeScene();
-    }
-
-    public void UpdateUpgradeScene()
-    {
-        FameAmount.text = PlayerUpgrades.Instance.Fame.ToString();
-        // If nothing selected, blank text fields and hide buy button
-        if (selectedUpgrade.upgrade == null)
+        void Start()
         {
-            SelectedName.text = "";
-            SelectedDescription.text = "";
-            PurchaseButton.gameObject.SetActive(false);
-            return;
+            Instance = this;
+            UpdateButtons();
+            UpdateUpgradeScene();
+        }
+        public void LoadTitleScene()
+        {
+            SceneManager.LoadScene("Title");
         }
 
-        // populate Name and description
-        SelectedName.text = selectedUpgrade.upgrade.upgradeName;
-        SelectedDescription.text = selectedUpgrade.upgrade.upgradeDescription[selectedUpgrade.currentRank];
-        // If max rank, hide button
-        if (selectedUpgrade.IsMaxed) PurchaseButton.gameObject.SetActive(false);
-        // else show button and update button with price
-        else
+        private void UpdateButtons()
         {
-            PurchaseButton.gameObject.SetActive(true);
-            PurchaseButton.GetComponentInChildren<TMP_Text>().text = selectedUpgrade.GetCostForNextRank().ToString();
+            foreach (UpgradeButton button in upgradeButtons) button.UpdateButton();
         }
-        // if can't afford, disable button
-        if (selectedUpgrade.CanAffordNextRank()) PurchaseButton.enabled = true;
-        // else enable button
-        else PurchaseButton.enabled = false;
+
+
+        public void SelectUpgrade(UpgradeProgress upgradeProgress)
+        {
+            selectedUpgrade = upgradeProgress;
+            UpdateUpgradeScene();
+        }
+
+        public void UpdateUpgradeScene()
+        {
+            fameAmount.text = PlayerUpgrades.Instance.Fame.ToString();
+            // If nothing selected, blank text fields and hide buy button
+            if (selectedUpgrade.upgrade == null)
+            {
+                selectedName.text = "";
+                selectedDescription.text = "";
+                purchaseButton.gameObject.SetActive(false);
+                return;
+            }
+
+            // populate Name and description
+            selectedName.text = selectedUpgrade.upgrade.upgradeName;
+            selectedDescription.text = selectedUpgrade.upgrade.upgradeDescription[selectedUpgrade.currentRank];
+            // If max rank, hide button
+            if (selectedUpgrade.IsMaxed) purchaseButton.gameObject.SetActive(false);
+            // else show button and update button with price
+            else
+            {
+                purchaseButton.gameObject.SetActive(true);
+                purchaseButton.GetComponentInChildren<TMP_Text>().text = selectedUpgrade.GetCostForNextRank().ToString();
+            }
+            // if can't afford, disable button
+            if (selectedUpgrade.CanAffordNextRank()) purchaseButton.enabled = true;
+            // else enable button
+            else purchaseButton.enabled = false;
+        }
+
+        public void PurchaseUpgrade()
+        {
+            // Deduct Fame
+            PlayerUpgrades.Instance.Fame -= selectedUpgrade.GetCostForNextRank();
+            // Rank up Progress
+            selectedUpgrade.IncreaseRank();
+
+            UpdateButtons();
+            UpdateUpgradeScene();
+        }
+
+        //Panel Methods
+        public void TogglePanels()
+        {
+            foreach (CanvasGroup panel in panels) Helper.CanvasHelper.ToggleCanvasGroup(panel);
+            foreach (Button button in toggleButtons) button.enabled = !button.enabled;
+
+            Color colorPlaceholder = tabImages[0].color;
+            tabImages[0].color = tabImages[1].color;
+            tabImages[1].color = colorPlaceholder;
+        }
     }
 
-    public void PurchaseUpgrade()
-    {
-        // Deduct Fame
-        PlayerUpgrades.Instance.Fame -= selectedUpgrade.GetCostForNextRank();
-        // Rank up Progress
-        selectedUpgrade.IncreaseRank();
 
-        UpdateButtons();
-        UpdateUpgradeScene();
-    }
 
-    //Panel Methods
-    public void TogglePanels()
-    {
-        foreach (CanvasGroup panel in panels) Helper.CanvasHelper.ToggleCanvasGroup(panel);
-        foreach (Button button in toggleButtons) button.enabled = !button.enabled;
-
-        Color colorPlaceholder = tabImages[0].color;
-        tabImages[0].color = tabImages[1].color;
-        tabImages[1].color = colorPlaceholder;
-    }
 }
-
-
-
